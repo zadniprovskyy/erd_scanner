88817f1039a7752f44a86ce6cb47657f118ca709
Sandy Wang <i@sandy.moe>
2020-07-08 07:25:09 -0400

    demo update

--- demo.py
+++ demo.py
@@ -28,12 +28,12 @@ if __name__ == "__main__":
     bwi = cv2.bitwise_not(orig_img)
     # find contours
 
-    node_contours, node_shapes, node_double_lined = get_node_contours_and_shapes(binary_img=binary_orig_img)
+    node_contours, node_shapes, node_double_lined,node_ocr = get_node_contours_and_shapes(binary_img=binary_orig_img)
 
     node_mask, edge_mask = get_node_and_edge_masks(binary_img=binary_orig_img, node_contours=node_contours)
     numpy_horizontal_concat = np.concatenate((node_mask, edge_mask), axis=1)
     cv2.imshow('numpy_horizontal_concat', numpy_horizontal_concat)
     G = get_graph_from_masks(edge_mask=edge_mask, node_contours=node_contours, node_shapes=node_shapes,
-                             node_double_lined=node_double_lined)
+                             node_double_lined=node_double_lined,node_OCR=node_ocr)
 
     draw_graph(G)
