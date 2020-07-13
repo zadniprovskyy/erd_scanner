125b627ae046cd5a79435ff34f458fa0a8e85af5
Yegor <yegor.zadniprovskyy@mail.utoronto.ca>
2020-07-13 12:37:04 -0700

    erenamed demo file

--- demos/demo.py
+++
@@ -1,9 +0,0 @@
-import os
-from erd_scanner.demo_utils import draw_graph
-from erd_scanner.img_to_graph import img_to_graph
-import cv2
-
-if __name__ == "__main__":
-    orig_img = cv2.imread(os.path.join("../imgs", "erd_sample_5.png"), cv2.IMREAD_GRAYSCALE)
-    G = img_to_graph(img=orig_img)
-    draw_graph(G)
